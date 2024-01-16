58be14fed75f05518661c6546555763269971f32
Olzhas Makhmet <mkhmtcore@gmail.com>
2024-01-16 22:06:42 +0600

    first commit

---
+++ Lab 1/Data Type/All.py
@@ -0,0 +1,7 @@
+# 1. int
+# 2. str
+# 3. float
+# 4. list
+# 5. tuple
+# 6. dict
+# 7. bool
---
+++ Lab 1/Variables/All exercises.py
@@ -0,0 +1,20 @@
+carname = "Volvo"
+x = 5
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = 10
+z = x + y
+print(z)
+
+x, y, z = "Banana", "somefruit", 'idk'
+
+
+x = y = z = "Orange"
+
+def func():
+    global x
+    x = "fantastic"
