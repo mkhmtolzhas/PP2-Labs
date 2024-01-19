ab7f8b8efbcddc109bba6570fcbcb06291e9652e
Olzhas Makhmet <mkhmtcore@gmail.com>
2024-01-19 10:45:49 +0600

    Show to Aruzhan

---
+++ Lab4/Datetime Ex/DateEx.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+# first_date = dt.date(2020, 10, 2)
+# second_date = dt.date(2020, 10, 7)
+# days_5 = dt.timedelta(5, 0, 0, 0, 0, 0, 0)
+# delta = second_date - first_date
+# print(delta)
+# print(dt.date.today() - delta)
+# print(print(type(delta)))
+print(dt.date.today() - dt.timedelta(5))
